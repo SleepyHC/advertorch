cb97fb801025b5aedd83c88b9d6e5b81697d48e2
SleepyHC <72695009+SleepyHC@users.noreply.github.com>
2023-03-25 23:14:01 +0800

    outpusISdist

--- advertorch/attacks/iterative_projected_gradient.py
+++ advertorch/attacks/iterative_projected_gradient.py
@@ -63,6 +63,11 @@ def perturb_iterative(xvar, yvar, predict, nb_iter, eps, eps_iter, loss_fn,
     delta.requires_grad_()
     for ii in range(nb_iter):
         outputs = predict(xvar + delta)
+        if isinstance(outputs, dict):
+            outputs = outputs['logits']
+            # div_output = outputs['div']
+        else:
+            outputs = outputs
         loss = loss_fn(outputs, yvar)
         if minimize:
             loss = -loss
