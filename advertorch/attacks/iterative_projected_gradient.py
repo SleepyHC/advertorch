6abfa55557e0f6793bb401cbfd3c9f5cd584830d
SleepyHC <72695009+SleepyHC@users.noreply.github.com>
2023-03-30 14:51:38 +0800

    debug

--- advertorch/attacks/iterative_projected_gradient.py
+++ advertorch/attacks/iterative_projected_gradient.py
@@ -68,8 +68,8 @@ def perturb_iterative(xvar, yvar, predict, nb_iter, eps, eps_iter, loss_fn,
             # div_output = outputs['div']
         else:
             outputs = outputs
-        loss = loss_fn(outputs, yvar).retain_grad()
-        loss.requires_grad = True
+        loss = loss_fn(outputs, yvar)
+        loss = Variable(loss, requires_grad = True).retain_grad()
         if minimize:
             loss = -loss
 
